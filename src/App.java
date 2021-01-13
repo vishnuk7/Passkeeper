b4aaa6a525a1a88f476cdd68b242dd871f049fd7
Vishnu K Raman <vishnu.kraman88@gmail.com>
2021-01-14 01:37:36 +0530

    Update App ♥

--- src/App.java
+++ src/App.java
@@ -4,13 +4,28 @@ import parameter.Options;
 import parameter.ParserArgs;
 
 public class App {
+    private static void parameter(Options op) {
+        switch (op.flag) {
+            case "-h":
+                System.out.println("call the help method");
+                break;
+        }
+
+    }
 
     public static void main(String[] args) {
         ParserArgs parserArgs = new ParserArgs(args);
         List<Options> optList = parserArgs.getOptList();
+        List<String> argsList = parserArgs.getArgsList();
 
-        // for (Options op : optList) {
-        //     parameter(op);
-        // }
+        System.out.println("hello");
+
+        for (Options op : optList) {
+            parameter(op);
+        }
+
+        for(String arg: argsList){
+            System.out.println("hee"+ arg);
+        }
     }
 }
